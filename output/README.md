cf277af006acc9ca6e60bddceb50b4ddac74f179
dymoy <148160802+dymoy@users.noreply.github.com>
2024-01-07 14:25:33 -0800

    Added Table of Contents section in output file.

--- output/README.md
+++ output/README.md
@@ -3,6 +3,14 @@
 ## Description
 This tool assists with generating a professional README markdown file for your project.
 
+## Table of Contents 
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution Guidelines](#contribution-guidelines)
+- [Testing](#testing)
+- [Questions](#questions)
+- [License](#License)
+
 ## Installation 
 N/A
 
@@ -16,8 +24,9 @@ N/A
 N/A
 
 ## Questions
-My GitHub profile can be found [here](https://github.com/dymoy)
-For any related questions, please contact me via email at dymoy@gmail.com
+This project was created by @dymoy  
+My GitHub profile can be found [here](https://github.com/dymoy)  
+For any related questions, please contact me via email at [derekymoy@gmail.com]
 
 ## License 
 This GitHub repository is licensed under the MIT license.
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,12 +10,20 @@ function renderLicenseLink(license) {}
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
+// Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
 ## Description
 ${data.description}
+
+## Table of Contents 
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution Guidelines](#contribution-guidelines)
+- [Testing](#testing)
+- [Questions](#questions)
+- [License](#License)
 
 ## Installation 
 ${data.installation}
@@ -30,8 +38,9 @@ ${data.contribution}
 ${data.test}
 
 ## Questions
-My GitHub profile can be found [here](https://github.com/${data.username})
-For any related questions, please contact me via email at ${data.email}
+This project was created by @${data.username}  
+My GitHub profile can be found [here](https://github.com/${data.username})  
+For any related questions, please contact me via email at [${data.email}]
 
 ## License 
 This GitHub repository is licensed under the ${data.license} license.
